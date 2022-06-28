a57fffe5f4d175c2e92b5196f2b718855fb35664
DuongXT <duong.nguyen0115@gmail.com>
2022-06-28 22:33:48 +0700

    duong0115
    first time

---
+++ android/app/src/main/kotlin/com/example/secrect_file/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.secrect_file
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ultis/AppUtils.dart
@@ -0,0 +1,4 @@
+class AppUtils {
+  static SharedPreferences sharedPreferences;
+}
+
